3b97649486c09b5a49ea1a85f772103909c77b6a
wisskers93 <cmwgonzales18@gmail.com>
2020-08-20 11:12:19 -0400

    added logos to slider - need to fix gallery buttons

--- portfolio.js
+++ portfolio.js
@@ -18,15 +18,47 @@ function myFunction() {
 
 // skill scroll
 
-// $(document).ready(()=> {
-//     $('#slideshow .slick').slick();
-// });
           
 $(function () {
     $('.autoplay').slick({
-        slidesToShow: 4,
+        slidesToShow: 6,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
       });
 });
+
+
+// gallery selection
+
+function changeImg1() {
+  var imgCaption = document.getElementById('imgCaption');
+  var image = document.getElementById('largeImg');
+  image.src = "/Images/theptcenter.png";
+  imgCaption.innerHTML = "<a href='https://www.physicaltherapycenter.org/' target='_blank'> Wordpress Website for The Physical Therapy Center </a>";
+}
+
+function changeImg2() {
+  var image = document.getElementById('largeImg');
+  image.src = "/Images/publicpolicy.png";
+}
+
+function changeImg3() {
+  var image = document.getElementById('largeImg');
+  image.src = "/Images/southerncharm.png";
+}
+
+function changeImg4() {
+  var image = document.getElementById('largeImg');
+  image.src = "/images/triangleface copy2.jpg";
+}
+
+function changeImg5() {
+  var image = document.getElementById('largeImg');
+  image.src = "/Images/BFBS Logo Splatters (Large).jpg";
+}
+
+function changeImg6() {
+  var image = document.getElementById('largeImg');
+  image.src = "/Images/CWG Web Designs.png";
+}
