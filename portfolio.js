34072b1bdd00d17a6c2e02dabcb3d1d3ab451ba3
wisskers93 <cmwgonzales18@gmail.com>
2020-08-21 11:07:40 -0400

    final edit

--- portfolio.js
+++ portfolio.js
@@ -1,13 +1,11 @@
-// When the user scrolls the page, execute myFunction
+//sticky navbar
+
 window.onscroll = function() {myFunction()};
 
-// Get the navbar
 var navbar = document.getElementById("navbar");
 
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var sticky = navbar.offsetTop +5;
 
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
